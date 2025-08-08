35935299cbaf3b0a8b9d32336d178956bffa06a5
JOSHUA ROTHE <joshuarothe@JOSHUAs-MacBook-Pro.local>
2025-08-08 11:29:06 -0400

    restored handler

--- contents/base/{{ project-name }}/src/routing/router.tsx
+++ contents/base/{{ project-name }}/src/routing/router.tsx
@@ -1,6 +1,7 @@
 import HomePage from '@/pages/home';
 import MainLayout from '@/pages/main-layout';
 import { createBrowserRouter } from '@modern-js/runtime/router';
+//import ContentHandler from '@/pages/content-handler';
 
 export const router = createBrowserRouter([
   {
@@ -12,10 +13,8 @@ export const router = createBrowserRouter([
         element: <HomePage />,
       },
       // {
-      //   "name": "App1",
-      //   "title": "Sample App 1",
-      //   "path": "/app1",
-      //   "componentPath": "app1/app1-content"
+      //   path: '/app1',
+      //   element: <ContentHandler name="App1" title="Sample App 1" componentPath="app1/app1-content" />,
       // }
     ],
   },
